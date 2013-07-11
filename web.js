6cb56ea680e63cf9785553c2f54f1bc1b1549b12
Shih-lex <lex_shih@payeasy.com.tw>
2013-07-11 19:07:35 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var fs= require('fs');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-
-app.get('/', function(request, response) {  var file = fs.readFileSync('./index.html','utf-8');  response.send(file.toString());});
-
+app.get('/', function(request, response) {  
+    var file = fs.readFileSync('./index.html','utf-8');
+    response.send(file.toString());
+    });
+  
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    app.listen(port, function() {  
+    console.log("Listening on " + port);
 });
